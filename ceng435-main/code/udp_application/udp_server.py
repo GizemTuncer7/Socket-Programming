62d17e012fbb4c27945ddcebc6b79a424a5e3d26
Taner Sarp Tonay <sarptonay813@gmail.com>
2024-01-04 16:56:59 +0300

    hıh

---
+++ ceng435-main/code/udp_application/udp_server.py
@@ -0,0 +1,5 @@
+from UDP_Server_with_Selective_Repeat import UDP_Server_with_Selective_Repeat
+
+if __name__ == '__main__':
+    udp_server = UDP_Server_with_Selective_Repeat()
+    udp_server.run()
