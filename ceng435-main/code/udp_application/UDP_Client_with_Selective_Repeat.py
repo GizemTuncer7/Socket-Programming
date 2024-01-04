eef68145b0307ca4df5d17344dc10671875d47d5
tanersarptonay <sarptonay813@gmail.com>
2024-01-05 00:58:02 +0300

    Non blocking eklendi

--- ceng435-main/code/udp_application/UDP_Client_with_Selective_Repeat.py
+++ ceng435-main/code/udp_application/UDP_Client_with_Selective_Repeat.py
@@ -46,7 +46,8 @@ class UDP_Client_with_Selective_Repeat:
 
     def send_data(self):
         # print("Send Data Window Length:", len(self.window))
-        if (self.next_sequence_number - self.send_base) < WINDOW_SIZE:
+        if  self.next_sequence_number < self.packets_length and (self.next_sequence_number - self.send_base) < WINDOW_SIZE:
+            # print("next_sequence_number:", self.next_sequence_number)
             self.packets[self.next_sequence_number].change_state_as_sent()
             self.UDPClientSocket.sendto(self.packets[self.next_sequence_number].packed_data_chunk_package(), self.serverAddressPort)
             self.next_sequence_number += 1
@@ -57,10 +58,10 @@ class UDP_Client_with_Selective_Repeat:
 
 
     def receive_ack(self):
-        ack, address = self.UDPClientSocket.recvfrom(BUFFER_SIZE)
-
-        if (ack == None):
-            raise Exception("Ack is None")
+        try:
+            ack, address = self.UDPClientSocket.recvfrom(BUFFER_SIZE)
+        except:
+            return
 
         packet_number, sequence_number, tag = unpack_ack(ack)
         # print(f"Received Ack: {sequence_number} - {tag}")
@@ -73,12 +74,14 @@ class UDP_Client_with_Selective_Repeat:
             self.send_base += 1
             if (self.send_base == self.packets_length):
                 self.is_finished = True
+
          
 
     def run(self):
         now = time.time()
 
         self.UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.UDPClientSocket.setblocking(0)
 
         interleaved_path_list = get_interleaved_path_list()
 
